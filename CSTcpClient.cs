e1affcf02e4735c0f5491dd1b7a679fd52a6f108
locple <131681238+locple@users.noreply.github.com>
2023-05-22 05:27:07 +0700

    Add support for both IPv4 & IPv6

--- CSTcpClient.cs
+++ CSTcpClient.cs
@@ -10,20 +10,35 @@ using System.Net.Sockets;
 
 public class CSTcpClient {
 	// Khai báo địa chỉ và port của server
-	static IPAddress ip = IPAddress.Parse("127.0.0.1");	// Server IP
-	static int port = 8000;								// Server Port
+	static string ip = "::1";						// Default Server IP
+	static string port = "9000";						// Default Server Port
 	// Khai báo bộ nhớ đệm để nhận data (32 bytea)
 	static byte[] buffer = new byte[32];
 	// Khai báo data
-	static string data = "20.5";	// USD
+	static string data;									// USD
 
-	public static void Main() {
+	public static void Main(string[] args) {
+		// Tham số dòng lệnh có dạng IPv4 hay IPv6 như sau:
+		// 127.0.0.1:8000
+		// ::1:8000
+		// [fe80::40ee:6de3:7926:d4bb]:8000
+		// fe80::34e3:97ee:f79a:c001:8000
+		if (args.Length > 0) {
+			int idx = args[0].LastIndexOf(':');
+			ip = args[0].Substring(0, idx);
+			port = args[0].Substring(idx + 1);
+		}
+
 		// Tạo client socket
 		Socket shipper = new Socket(SocketType.Stream, ProtocolType.Tcp);
 		// Kết nối đến server tại ip:port
-		IPEndPoint address = new IPEndPoint(ip, port);
+		IPEndPoint address = new IPEndPoint(IPAddress.Parse(ip), int.Parse(port));
 		shipper.Connect(address);
 		Console.WriteLine("Connected to server.");
+
+		// Nhập số USD cần quy đổi
+		Console.Write("Nhap so USD: ");
+		data = Console.ReadLine();
 
 		// Gửi data cho server
 		shipper.Send(Encoding.ASCII.GetBytes(data));
