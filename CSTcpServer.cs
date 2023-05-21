bffeb9a651eefdb938f8ef4ae170e8ba9e670bc0
locple <131681238+locple@users.noreply.github.com>
2023-05-22 05:25:54 +0700

    Add support for both IPv4 & IPv6

--- CSTcpServer.cs
+++ CSTcpServer.cs
@@ -7,27 +7,18 @@ using System;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 public class CSTcpServer {
-	// Khai báo port của server
-	static int port = 8000;
-	// Khai báo bộ nhớ đệm để nhận data (32 bytes)
-	static byte[] buffer = new byte[32];
+	// Khai báo port mặc định của server
+	static string port = "9000";
 	// Khai báo dữ liệu của server
 	static double rate = 24640;		// Tỉ giá VND/USD
 
-	public static void Main() {
-		// Tạo server socket
-		Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-		// Gán server tại port
-		IPEndPoint address = new IPEndPoint(IPAddress.Any, port);
-		listener.Bind(address);
-		// Mở server để chờ kết nối đến
-		listener.Listen(3);			// Số client tối đa cho phép cùng lúc = 3
-		Console.WriteLine("C# Server listening...");
-		// Tạo socket khi có client kết nối đến
-		Socket shipper = listener.Accept();
-		Console.WriteLine("Client connected.");
+	static void Serve(Object obj) {
+		// Khai báo bộ nhớ đệm để nhận data (32 bytes)
+		byte[] buffer = new byte[32];
+		Socket shipper = (Socket)obj;
 
 		// Nhận data từ client gửi đến
 		int len = shipper.Receive(buffer);
@@ -41,8 +32,36 @@ public class CSTcpServer {
 		shipper.Send(Encoding.ASCII.GetBytes(data));
 		Console.WriteLine("Data sent back: {0}", data);
 
-		// Đóng các socket
+		// Đóng socket nối với client
 		shipper.Shutdown(SocketShutdown.Both);
+	}
+
+	public static void Main(string[] args) {
+		// Lấy tham số port từ dòng lệnh
+		if (args.Length > 0) port = args[0];
+
+		// Tạo server socket
+		///Socket listener = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+		///listener.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
+		Socket listener = new Socket(SocketType.Stream, ProtocolType.Tcp);
+		// Gán server tại mọi IPv4 & IPv6 : port
+		IPEndPoint address = new IPEndPoint(IPAddress.IPv6Any, int.Parse(port));
+		listener.Bind(address);
+		// Mở server để chờ kết nối đến
+		listener.Listen(0);			// Max số client tối đa cho phép cùng lúc
+		Console.WriteLine("C# Server listening at port {0}...", port);
+
+		// Xử lý cho từng client kết nối
+		while (!Console.KeyAvailable) {
+			// Tạo socket khi có client kết nối đến
+			Socket shipper = listener.Accept();
+			Console.WriteLine("Client connected.");
+
+			Thread t = new Thread(new ParameterizedThreadStart(Serve));
+			t.Start(shipper);
+		};
+
+		// Đóng socket lắng nghe
 		listener.Close();
 	}
 }
